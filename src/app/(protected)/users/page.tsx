902866bdafe64bf5fe17d6b4ee4a1b21ddad7645
jerrychng <jerrychng.yingjie@aspect.co.uk>
2026-02-23 03:41:09 +0000

    style

--- src/app/(protected)/users/page.tsx
+++ src/app/(protected)/users/page.tsx
@@ -197,7 +197,7 @@ export default function EngineerPage() {
             "Cancel"
           ) : (
             <>
-             
+              <Image src="/assets/Plus_rectangle.svg" alt="" width={16} height={16} className="h-4 w-4" aria-hidden />
               Create engineer
             </>
           )}
@@ -286,7 +286,7 @@ export default function EngineerPage() {
                   "Creating..."
                 ) : (
                   <>
-                    
+                    <Image src="/assets/Plus_rectangle.svg" alt="" width={16} height={16} className="h-4 w-4" aria-hidden />
                     Create engineer
                   </>
                 )}
